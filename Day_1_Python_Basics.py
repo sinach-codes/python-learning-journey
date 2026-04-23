dff897f7338288a5953fa61b8e9641ed53f35a65
Izuehie ThankGod <izuehiethankgod@gmail.com>
2026-04-23 08:39:47 -0700

    Create Day_1_Python_Basics.py
    Day 1 completed band naming generator

---
+++ Day_1_Python_Basics.py
@@ -0,0 +1,9 @@
+# Day 1 - Python Basics (Angela Yu Course)
+
+# What i learned:
+# - print function
+# - strings manipulation
+# - inputs
+# -variables
+# -variable naming
+# -band name generator project
